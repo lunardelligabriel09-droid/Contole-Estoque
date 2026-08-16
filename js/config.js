31d87e1500f966ba666d7a08338691772b4f305c
lunardelligabriel09-droid <lunardelli.gabriel09@gmail.com>
2026-08-16 22:07:32 +0000

    finalizando o js, ainda com problemas, adicionando o config e login

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// Ponto UNICO de configuracao do endereco da API.
+// Para trocar o servidor, altere apenas esta linha.
+// No celular (Expo/rede), troque &quot;localhost&quot; pelo IP da maquina.
+const API_URL = 'http://localhost:3000';
