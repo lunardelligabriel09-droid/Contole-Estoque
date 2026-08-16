d297d1edc96929a0ee634744e60a28dec12a63c1
lunardelligabriel09-droid <lunardelli.gabriel09@gmail.com>
2026-08-16 21:17:40 +0000

    criandos as rotas, AuthRoutes

---
+++ back-end/src/routes/authRoutes.js
@@ -0,0 +1,7 @@
+// Rotas de autenticacao (publicas).
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers/authController');
+router.post('/registrar', ctrl.registrar);
+router.post('/login', ctrl.login);
+module.exports = router;
